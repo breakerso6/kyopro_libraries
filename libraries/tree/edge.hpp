0b331de5f74090980daf8c1dd0cff6d94863e3d2
Carbon_so <math5923tan@gmail.com>
2026-07-10 13:05:17 +0900

    重みでの比較を追加

--- libraries/tree/edge.hpp
+++ libraries/tree/edge.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <tuple>
+
 template <class S> struct edge {
     int from, to;
     S weight;
     edge(int from_, int to_, S weight_) : from(from_), to(to_), weight(weight_) {}
     edge() : from(-1), to(-1), weight() {}
+    bool operator<(const edge& other) const {
+        return std::tie(weight, from, to) < std::tie(other.weight, other.from, other.to);
+    }
 };
